2ddd181226f6e28dfbf37410cd45233d9239bf4d
Ahmed Dawoud <151741341+Economist-Ahmed-Dawoud@users.noreply.github.com>
2024-08-10 01:01:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🚀 Cuasal ML')
 
 st.info('By leveraging counterfactual causal reasoning and advanced machine learning techniques, we seek to provide deep insights into the dynamics of the digital freelance economy.')
+
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/Economist-Ahmed-Dawoud/Data/main/streamlit-upwork-data.csv')
+  df
+  
