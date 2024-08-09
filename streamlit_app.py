8be25d14158c8fef9ab1e351c96641ab671013ea
Ahmed Dawoud <151741341+Economist-Ahmed-Dawoud@users.noreply.github.com>
2024-08-10 00:49:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('Cuasal ML')
+st.title('🚀 Cuasal ML')
 
-st.info('This research project aims to uncover the causal factors influencing freelancer success on Upwork, focusing on profiles from India and Egypt. By leveraging counterfactual causal reasoning and advanced machine learning techniques, we seek to provide deep insights into the dynamics of the digital freelance economy.')
+st.info('By leveraging counterfactual causal reasoning and advanced machine learning techniques, we seek to provide deep insights into the dynamics of the digital freelance economy.')
